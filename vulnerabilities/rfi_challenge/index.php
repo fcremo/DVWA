c092b94da823085f15267dbe5f457a123c8c8a56
fcremo (Filippo Cremonese) <fcremo@users.noreply.github.com>
2016-10-28 00:41:43 +0200

    Added RFI challenge

---
+++ vulnerabilities/rfi_challenge/index.php
@@ -0,0 +1,35 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '../../' );
+require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
+
+dvwaPageStartup( array( 'authenticated', 'phpids' ) );
+
+$page = dvwaPageNewGrab();
+$page[ 'title' ]   = 'Challenge: File Inclusion' . $page[ 'title_separator' ].$page[ 'title' ];
+$page[ 'page_id' ] = 'rfi_challenge';
+$page[ 'help_button' ]   = 'rfi_challenge';
+$page[ 'source_button' ] = 'rfi_challenge';
+
+dvwaDatabaseConnect();
+
+$vulnerabilityFile = '';
+switch( $_COOKIE[ 'security' ] ) {
+	default:
+		$vulnerabilityFile = 'challenge.php';
+		break;
+}
+
+require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/fi/source/{$vulnerabilityFile}";
+
+// if( count( $_GET ) )
+if( isset( $file ) )
+	include( $file );
+else {
+	header( 'Location:?page=include.php' );
+	exit;
+}
+
+dvwaHtmlEcho( $page );
+
+?>
---
+++ vulnerabilities/rfi_challenge/source/challenge.php
@@ -0,0 +1,24 @@
+<?php
+
+// The page we wish to display
+$file = $_GET[ 'page' ];
+
+function endswith($haystack, $needle){
+    $length = strlen($needle);
+    if ($length == 0) {
+            return true;
+    }
+
+    return (substr($haystack, -$length) === $needle);
+}
+
+
+if(!endswith($file, ".php")){
+    $file = $file . ".php";
+}
+
+// Input validation
+$file = str_replace( array( "http://", "https://" ), "", $file );
+$file = str_replace( array( "../", "..\"" ), "", $file );
+
+?>
